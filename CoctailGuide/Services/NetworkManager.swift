0234fd5db9583da4e39e896f84bef58825c6ef26
SergeyZakurakin <pricezak@gmail.com>
2025-08-03 14:48:58 -0400

    setup Api Request

--- CoctailGuide/Services/NetworkManager.swift
+++ CoctailGuide/Services/NetworkManager.swift
@@ -8,3 +8,37 @@
 import Foundation
 
 
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private let baseURL = "https://api.api-ninjas.com/v1/cocktail?name="
+    private let apiKey = "cICqhm5A062WSKAdEKbMeQ==ENY5vDAhsTGiJ9XI"
+    
+    private init() {}
+    
+    func fetchCoctails(name: String) async -> [Coctail] {
+        let query = name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+             let fullURLString = baseURL + query
+        
+        guard let url = URL(string: fullURLString) else {
+            print("invalid URL")
+            return []
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue(apiKey, forHTTPHeaderField: "X-Api-Key")
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            
+            let decoded = try JSONDecoder().decode([Coctail].self, from: data)
+            return decoded
+            
+        } catch {
+            print("Error fetching cocktails: \(error.localizedDescription)")
+                       return []
+        }
+    }
+}
