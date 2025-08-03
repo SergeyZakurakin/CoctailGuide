d7f887d1a398d2c171d8b892a12bdfffd124bba8
SergeyZakurakin <pricezak@gmail.com>
2025-08-03 14:13:56 -0400

    Initial Commit

---
+++ CoctailGuide/CoctailGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoctailGuideApp.swift
+//  CoctailGuide
+//
+//  Created by Sergey Zakurakin on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CoctailGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
